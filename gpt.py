6d70b25e2ed65a0c4b20ed9f07005225f9548af8
Sushi22 <sushmitajoshi22@gmail.com>
2024-04-18 16:17:09 +0530

    modified readme

--- gpt.py
+++ gpt.py
@@ -1,13 +1,13 @@
 import openai
 
-api_key = "f5b55c3f2f504eb3a63c1875b0241ce2"
+api_key = ""
 openai.api_key = api_key
 
 endpoint = "https://sysops-28015.openai.azure.com/"
 
 question = "What is the capital of France?"
 response = openai.Completion.create(
-  engine="text-davinci-003", 
+  engine="gpt-4-turbo", 
   prompt=question,
   max_tokens=50 
 )
--- scrape.py
+++ scrape.py
@@ -4,29 +4,21 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
 
-# Set up Selenium
 options = Options()
-options.add_argument("--headless")  # Run headless browser
-driver = webdriver.Chrome(options=options)  # Make sure you have chromedriver installed and in your PATH
+options.add_argument("--headless")  
+driver = webdriver.Chrome(options=options)  
 driver.get("https://cikibana.myntra.com/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(columns:!(),filters:!(),index:pimservice,interval:auto,query:(language:kuery,query:''),sort:!(!('@timestamp',desc)))")
 
-# Wait for the page to load
-time.sleep(5)  # You may need to adjust this depending on your internet speed
+time.sleep(5)  
 
-# Get the page source
 page_source = driver.page_source
 
 element_xpath = "/html/body/div[1]/div[2]/div[2]/div/div/div/div[1]/div/div/div[1]/div/div[1]/div/div/button/span/span"
+
 element = driver.find_element_by_xpath(element_xpath)
 
-# Click on the element
 element.click()
 
-# Parse the page source with BeautifulSoup
 soup = BeautifulSoup(page_source, "html.parser")
 
-# Now you can extract the data you need from the BeautifulSoup object
-# For example, you can find elements by their class or tag and extract text
-
-# Close the browser
 driver.quit()
